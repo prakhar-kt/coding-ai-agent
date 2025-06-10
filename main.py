76f0728deb47f9d655d9d919ee3cb1aaa281b043
prakhar-kt <rahkarp@gmail.com>
2025-06-10 16:21:01 +0530

    add multi promt support

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 
@@ -9,15 +10,24 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 try:
-    prompt = sys.argv[1]
+    user_prompt = sys.argv[1]
 except:
     raise Exception("No prompt provided")
 
+messages = [
+    types.Content(role="user",
+                  parts=[types.Part(text=user_prompt)])
+]
+
 response = client.models.generate_content(
     model = "gemini-2.0-flash-001",
-    contents = prompt
+    contents = messages
 )
-
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+try: 
+    verbose_flag = sys.argv[2]
+    print(f"User prompt: {user_prompt}")
+    # print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+except:
+    print(response.text)
